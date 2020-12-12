6d8aa4c3c38f28641b855aeaff65ba09026f3291
LaquiosDev <68591001+LaquiosDev@users.noreply.github.com>
2020-12-12 12:04:56 +0100

    Update ready.js

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 
 module.exports = (client) => {
 
-
     console.log(`${client.user.username} est en ligne`);
 
     client.user.setActivity("Made by Laquios | .help", {type: "LISTENING"});
