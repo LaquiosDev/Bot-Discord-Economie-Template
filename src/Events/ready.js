2deb31376955e20320f7e111e94e9c6f298da92d
LaquiosDev <68591001+LaquiosDev@users.noreply.github.com>
2020-12-12 12:04:04 +0100

    Update ready.js

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -5,17 +5,6 @@ module.exports = (client) => {
 
     console.log(`${client.user.username} est en ligne`);
 
-    //client.user.setActivity("/help", {type: "LISTENING"});
-
-    let statuses = [
-        //`la Maintenance`
-        `gerer l'economies de SP Developpement`,
-        `${client.users.cache.size} membres | spe.help`
-    ]
-
-    setInterval(function() {
-        let status = statuses[Math.floor(Math.random() * statuses.length)];
-        client.user.setActivity(status, {type: "PLAYING"}); 
-
-    }, 5000);
+    client.user.setActivity("Made by Laquios | .help", {type: "LISTENING"});
+    
 }
