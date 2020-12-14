03ed2159a31328216d1797650a342a43f3662c4c
LaquiosDev <68591001+LaquiosDev@users.noreply.github.com>
2020-12-14 06:55:51 +0100

    Update pay.js

--- src/Commandes/pay.js
+++ src/Commandes/pay.js
@@ -1,33 +1,36 @@
-  
 const Discord = require('discord.js')
 const db = require('quick.db')
 
 module.exports.run = async (client, message, args, config) => {
 
-
-
-    let user = message.mentions.members.first() 
-
-    let member = db.fetch(`money_${message.guild.id}_${message.author.id}`)
-
-
-    if (!user) {
-        return message.channel.send('Veuillez presisez une personne.')
-    }
-    if (!args[1]) {
-        return message.channel.send('Veuillez specifiez la sommes.')
-    }
-    if (message.content.includes('-')) { // if the message includes "-" do this.
-        return message.channel.send(`Impossible d'envoyer un chiffre negatif !`)
+    let user = message.mentions.members.first() || message.guild.members.find(gm => gm.id == args[0]);
+    let member = db.fetch(`money_${message.guild.id}_${message.author.id}`);
+  
+    let transaction = {
+      amount: parseInt(args[1]),
     }
 
-    if (member < args[1]) {
-        return message.channel.send("Tu n'as pas d'argent, ``spe.work`` pour en avoir")
+    if (!user) 
+        return message.channel.send('Veuillez preciser une personne à qui transférer de l\'argent. (Exemple `spe.pay @membre 100`)')
+    
+    if (!transaction.amount)
+        return message.channel.send('Veuillez specifier le montant de la transaction. (Exemple `spe.pay @membre 100`)')
+    
+    if (transaction.amount <= 0) { // Only positive amounts can be transferred
+      
+        if(transaction.amount < 0)
+          return message.channel.send(`Impossible d'envoyer un montant negatif !`);
+        else
+          return message.channel.send(`Vous ne pouvez pas envoyer 0$, réésayez en utilisant une somme positive.`);
+
+      
+    if (member < transaction.amount) {
+        return message.channel.send("Tu n'as pas suffisamment d'argent pour effectuer la transaction... Tapes ``spe.work`` pour en obtenir plus !")
     }
 
-    message.channel.send(`${message.author.tag} vien d'envoyer **__${args[1]}__** à **__${user.user.username}__** !`)
-    db.add(`money_${message.guild.id}_${user.id}`, args[1])
-    db.subtract(`money_${message.guild.id}_${message.author.id}`, args[1])
+    message.channel.send(`${message.author.tag} viens d'envoyer **__${transaction.amount}__** à **__${user.user.username}__** !`);
+    db.add(`money_${message.guild.id}_${user.id}`, transaction.amount)
+    db.subtract(`money_${message.guild.id}_${message.author.id}`, transaction.amount)
 
 }
 
